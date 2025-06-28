1d2866b7bab169e4791b6588ac2957c985c7e746
Kholin Nikita <nikitaholin11@gmail.com>
2025-06-28 08:08:02 +0300

    added model

---
+++ readme.md
@@ -0,0 +1,165 @@
+# Вариант
+`alg | risc | neum | hw | tick | binary | trap | mem | pstr | prob1 | vector`
+
+#### Холин Никита Сергеевич, Р3217, 409801
+
+### Оглавление
+
+# Язык программирования
+``` ebnf
+<letter>        ::= "A"…"Z" | "a"…"z" | "_"
+<digit>         ::= "0"…"9"
+<identifier>    ::= <letter> { <letter> | <digit> }
+
+<int>           ::= <digit> { <digit> }
+<array>         ::= <digit> { <digit> }
+<string>        ::= <int>'<любые печатные символы>'
+
+<var>           ::= string <identifier> = <string>; | int <identifier> = <int>; 
+                                                  | array <identifier> = <int>;
+
+<print>         ::= print(<identifier>);
+<if>            ::= if (<expression>) {<любой код>}
+<while>         ::= while (<expression>) {<любой код>}
+
+<interruption>  ::= interruption
+```
+
+## Семантика
+### Стратегия вычисления:
+- Программа выполняется последовательно 
+- Переменные должны быть объявлены до использования
+- Промежуточные результаты вычислений сохраняются в регистры G1 - G6
+- При возникновении прерывания, начинает исполнятся код, расположенный после метки interruption
+- Область видимости переменной - все переменные глобальные
+- Переменные типа int - изменяемые, типа string - константы
+
+
+## Организация памяти
+- Общая память для команд и данных
+- Размер машинного слова - 32 бита
+- В памяти с адреса 0xС располагаются данные, сразу после начинаются команды
+- Первые 12 ячеек занят под ввод/вывод и сохранение регистров при прерывании.
+- Big-endian
+- Ячейка 0x0 - ввод, 0х1 - вывод
+- Виды адресации - прямая и косвенная
+- Нет динамической памяти, вся память выделена до исполнения программы.
+
+
+## Регистры
+- `G1..G6,A,B,BR` - Регистры общего назначения
+- `PC` - program counter
+- `AR` - регистр адреса для обращения к памяти
+- `INT` - регистр в котором всегда хранится начало обработчика прерывания
+- `INTPC` - регистр в который сохраняется PC перед вызовом обработчика прерывания
+
+
+# Система команд
+- Длина инструкции 32 бита
+### Форматы: 
+Команды без обращения к памяти:
+- `|4 bit OPCODE|20 bit 0|4 bit r1|4 bit r2|`
+
+Команда JMP (безусловный переход):
+- `|4 bit OPCODE|0111|24 bit address|`
+
+Загрузка значения в регистр:
+- `|4 bit OPCODE|1000|20 bit immediate value|4 bit registr|`
+
+Загрузка в память:
+- `|4 bit OPCODE|1110|20 bit memory address |4 bit registr|`
+
+Чтение из памяти:
+- `|4 bit OPCODE|1010|20 bit memory address |4 bit registr|`
+
+Косвенная загрузка:
+- `|4 bit OPCODE|1111|16 bit 0|4 bit address r1|4 bit r2|`
+
+Косвенное чтение:
+- `|4 bit OPCODE|1011|16 bit 0|4 bit address r1|4 bit r2|`
+
+
+
+
+### Описание команд:
+-    `JMPE r1 r2: if r1 == r2 -> PC += 1`
+-    `JMP  addr: PC = addr  `
+-  `  JMPL r1 r2: if r1 < r2 -> PC += 1`
+-   ` AND  r1 r2: r1 = r1 & r2`
+-    `NOT  r1:    r1 = ~r1`
+-    `MUL  r1 r2: r1 = r1 * r2` 
+-    `OR   r1 r2: r1 = r1 | r2`
+-    `SUB  r1 r2: r1 = r1 - r2`
+-    `ADD  r1 r2: r1 = r1 + r2`
+-    `MOV  r1 r2: r1 = r2`
+-    `DIV  r1 r2: r1 = r1/r2`
+-    `LOAD r1 r2: r1 = MEM(r2)`
+-    `LOAD r1 addr: r1 = MEM(addr)`
+-    `STORE r1 r2: MEM(r2) = r1`
+-    `STORE r1 addr: MEM(addr) = r1`
+-    `LOADIM r1 val: r1 = val`
+-    `HALT `
+
+# Транслятор
+### Запуск: 
+`python translator.py [input_file] [out_file]`
+
+### Пример:
+![alt text](https://sun9-17.userapi.com/s/v1/if2/N3Bwhw3Pw9zTdJpNrZfr_rMgLfmGNPnGOi6pbX73H1_VaWwp5Jjtf90WQcs7vVO2Ak7ReeN6wfJNva9taEkhbd7d.jpg?quality=95&as=32x3,48x4,72x6,108x9,160x13,240x20,360x30,480x40,540x45,640x54,720x60,1080x90,1122x94&from=bu&cs=1122x0)
+
+### Запуск транслятора:
+![alt text](https://sun9-27.userapi.com/s/v1/if2/HOds2fCH_cnLEyC62r-LqvCATfwQH8__q-Xt1i1pQqmgJXIWAxFymXBkH_s9SVlfqWtJE_lMVxQe0V2YueGhJ_52.jpg?quality=95&as=32x5,48x7,72x10,108x15,160x23,240x34,360x52,480x69,540x78,640x92,720x103,822x118&from=bu&cs=822x0)
+
+### Результат:
+
+```
+0. 3800000f - LOADIM
+1. 380000b7 - LOADIM
+2. 2E0000c7 - STORE
+3. 38000487 - LOADIM
+4. 2E0000d7 - STORE
+5. 38000657 - LOADIM
+6. 2E0000e7 - STORE
+7. 380006c7 - LOADIM
+8. 2E0000f7 - STORE
+9. 380006c7 - LOADIM
+10. 2E000107 - STORE
+11. 380006f7 - LOADIM
+12. 2E000117 - STORE
+13. 38000207 - LOADIM
+14. 2E000127 - STORE
+15. 38000577 - LOADIM
+16. 2E000137 - STORE
+17. 380006f7 - LOADIM
+18. 2E000147 - STORE
+19. 38000727 - LOADIM
+20. 2E000157 - STORE
+21. 380006c7 - LOADIM
+22. 2E000167 - STORE
+23. 38000647 - LOADIM
+24. 2E000177 - STORE
+25. 1A0000c0 - LOAD
+26. 380000c1 - LOADIM
+27. 38000012 - LOADIM
+28. 38000007 - LOADIM
+29. 40000070 - JMPL
+30. 97000024 - JMP
+31. d0000072 - ADD
+32. d0000012 - ADD
+33. 1B000013 - LOAD
+34. 2E000013 - STORE
+35. 9700001d - JMP
+36. 50000000 - HALT
+```
+
+
+### Принцип работы:
+Весь данный код приводится к удобному для трансляции виду: между ключевыми словами и операторами ставятся пробелы, все выражения вида `a - b` заменяются на `a + -b` для простоты арифметики. После код разбивается на строчки, каждая строчка на токены и в соответсвии с этим происходит последовательный перевод каждой строки в машинный код.
+
+# Модель процессора
+
+## Data Path
+![alt text](https://sun9-36.userapi.com/s/v1/if2/7jGG40CUcgE3XcT8pRzd736x5iGL5STZf7-5wEguCJPId2oJUbZHF2t_NJ9fKhN7SgqVd0iw7e-oHvSlyMXftLFC.jpg?quality=95&as=32x25,48x37,72x56,108x84,160x124,240x186,360x279,480x372,540x418,640x495,720x557,1054x816&from=bu&cs=1054x0)
+
+## Control Unit
+![alt text](https://sun9-31.userapi.com/s/v1/if2/DToA99Ceer3OgnY9LgHS2DBLEJEm_RKzzAM9nsLPFcYWvGcWL0i-Jyl--KhYfSGNbX5eX35aRY4-RBpoexq1WfHW.jpg?quality=95&as=32x27,48x40,72x60,108x89,160x133,240x199,360x298,480x398,540x448,640x530,720x597,870x721&from=bu&cs=870x0)
--- translator.py
+++ translator.py
@@ -1,7 +1,8 @@
 import re
+import sys
 from isa import *
 
-reserved_keys = ['==','=','<','+','-','*','/','&','|','~','int','string','print','{','}','(',')','while','interruption','input']
+reserved_keys = ['==','=','!=','<','+','-','*','/','&','|','~','int','string','array','print','{','}','(',')','while','interruption','input']
 
 def read_file(path):
     res = ""
@@ -82,14 +83,32 @@ def token_lable_or_val(token,labels,register,line_number,line):
         return code
     except Exception:
         pass
+    
+    offset = 0 
+    if '[' in token and ']' in token:
+        try: 
+             offset = token.split('[')[1].split(']')[0]
+        except Exception:
+          raise ValueError(f"{line}\nIncorrect expresion on line {line_number}")
+
+    token = token.split('[')[0]
+
     if token in labels:
-        code.append(generate_machine_code(Opcode.LOAD,register,None,addr=labels.index(token)))
+        if offset in labels:
+            code.append(generate_machine_code(Opcode.LOAD,Registers.BR,None,addr=labels.index(offset)))
+            code.append(generate_machine_code(Opcode.LOADIM,Registers.A,None,1))
+            code.append(generate_machine_code(Opcode.ADD,Registers.BR,Registers.A))
+            code.append(generate_machine_code(Opcode.LOADIM,Registers.B,None,val=labels.index(token)))
+            code.append(generate_machine_code(Opcode.ADD,Registers.BR,Registers.B))
+            code.append(generate_machine_code(Opcode.LOAD,Registers.BR,register))
+        else:   
+            code.append(generate_machine_code(Opcode.LOAD,register,None,addr=labels.index(token) + int(offset)))
     elif (token[0] == '-' and token[1:] in labels):
-        code.append(generate_machine_code(Opcode.LOAD,Registers.G6,None,addr=labels.index(token[1:])))
+        code.append(generate_machine_code(Opcode.LOAD,Registers.G6,None,addr=labels.index(token[1:]) + int(offset)))
         code.append(generate_machine_code(Opcode.LOADIM,register,None,0))
         code.append(generate_machine_code(Opcode.SUB,register,Registers.G6))
     elif (token[0] == '~' and token[1:] in labels):
-        code.append(generate_machine_code(Opcode.LOAD,Registers.G6,None,addr=labels.index(token[1:])))
+        code.append(generate_machine_code(Opcode.LOAD,Registers.G6,None,addr=labels.index(token[1:]) + int(offset)))
         code.append(generate_machine_code(Opcode.NOT,register,Registers.G6))
     else:
         raise ValueError(f"{line}\nIncorrect expresion on line {line_number}")
@@ -102,7 +121,6 @@ def token_lable_or_val(token,labels,register,line_number,line):
 def count_expresion(expression:str,labels,line,line_number = 0):
     code = []   
     tree = [[[ [[i.strip().rstrip() for i in ili.split("&")] for ili in div.split("|")]  for div in mul.split("/")] for mul in plus.split("*")] for plus in expression.split("+")]
-    print(tree)
     code.append(generate_machine_code(Opcode.LOADIM,Registers.G1,None,0))
     for pls in tree:
         code.append(generate_machine_code(Opcode.LOADIM,Registers.G2,None,1))
@@ -125,8 +143,6 @@ def count_expresion(expression:str,labels,line,line_number = 0):
 
     return code
 
-
-# Это кринж! всё криво, но в задание же просят топорно 
 def translate(text):
     lines_temp = text.split(";")
     lines_temp2 = []
@@ -148,11 +164,14 @@ def translate(text):
         else:
             lines.append(line)
     lines = [i.strip().rstrip() for i in lines if len(i.strip().rstrip()) > 0]
-    print(lines)
     line_num = 0
-    code = []
+    code = [generate_machine_code(Opcode.LOADIM,Registers.INT,None,val = 0)]
     labels = ['input','output']   #резервируем для ввода-вывода  
-    type_of_labels = ['I','O']  # char or int
+    type_of_labels = ['I','O']  
+    for i in range(10): # резервируем 10 слов для сохранения регистров
+        labels.append('R')
+        type_of_labels.append('int')
+    
     branch_stack = []
     int_func_present = False
     for line in lines:
@@ -179,7 +198,7 @@ def translate(text):
             if name in labels:
                  raise ValueError(f"{line}\nRedeclaration on line {line_num}")
             
-            raw_string = symbols[3].split("'")
+            raw_string = line.split("=")[1].split("'")
             if (len(raw_string) < 3):
                 raise ValueError(f"{line}\nInvalid expresion on line {line_num}")
             
@@ -196,14 +215,45 @@ def translate(text):
                  code.append(generate_machine_code(Opcode.LOADIM,Registers.BR,None,val[i]))
                 else:
                  code.append(generate_machine_code(Opcode.LOADIM,Registers.BR,None,0))
-                code.append(generate_machine_code(Opcode.STORE,Registers.BR,None,addr=len(labels)-1))  
-        elif symbols[0] in labels:
+                code.append(generate_machine_code(Opcode.STORE,Registers.BR,None,addr=len(labels)-1)) 
+        elif symbols[0] == 'array':
+            if (len(symbols) < 3 or not '=' in line):
+                  raise ValueError("{line}\nIncorrect declaration on line {line_num}")
+            name = symbols[1]
+            lenght = int(symbols[3])
+            if name in labels:
+                 raise ValueError(f"{line}\nRedeclaration on line {line_num}")    
+            labels.append(name)
+            type_of_labels.append("array")
+            code.append(generate_machine_code(Opcode.LOADIM,Registers.BR,None,lenght))
+            code.append(generate_machine_code(Opcode.STORE,Registers.BR,None,addr=len(labels)-1))
+            for i in range(lenght):
+             labels.append(name)
+             type_of_labels.append("int")
+        elif symbols[0].split('[')[0] in labels:
              if (len(symbols) < 3 or not '=' in line):
                   raise ValueError(f"{line}\nIncorrect expresion on line {line_num}")
+             offset = 0 
+             if '[' in symbols[0] and ']' in symbols[0]:
+              try: 
+                 offset = symbols[0].split('[')[1].split(']')[0]
+              except Exception:
+                 raise ValueError(f"{line}\nIncorrect expresion on line {line_num}")
+             symbols[0] = symbols[0].split('[')[0]
              code += count_expresion(line.split('=')[1],labels,line,line_num)
-             code.append(generate_machine_code(Opcode.STORE,Registers.G1,None,addr=labels.index(symbols[0])))
+             if offset in labels:  
+                code.append(generate_machine_code(Opcode.LOAD,Registers.BR,None,addr=labels.index(offset)))
+                code.append(generate_machine_code(Opcode.LOADIM,Registers.A,None,1))
+                code.append(generate_machine_code(Opcode.ADD,Registers.BR,Registers.A))
+                code.append(generate_machine_code(Opcode.LOADIM,Registers.B,None,val=labels.index(symbols[0])))
+                code.append(generate_machine_code(Opcode.ADD,Registers.BR,Registers.B))
+                code.append(generate_machine_code(Opcode.STORE,Registers.BR,Registers.G1))
+             else:   
+                offset = int(offset)
+                if type_of_labels[labels.index(symbols[0])] == 'array': offset += 1
+                code.append(generate_machine_code(Opcode.STORE,Registers.G1,None,addr=labels.index(symbols[0]) + offset))
         elif symbols[0] == 'if' or symbols[0] == 'while':
-             operands = ['==','<']
+             operands = ['==','<','!=']
              if symbols[-1] != '{':
                     raise ValueError(f"{line}\nExpected {"{"} on line {line_num}")
              if symbols[1] != '(':
@@ -215,24 +265,28 @@ def translate(text):
                      break
 
              left_expression, right_expression = line.split('(')[1].split(')')[0].rstrip().strip().split(operand)
+             comeback = len(code)
              code += count_expresion(right_expression,labels,line,line_num)
              code.append(generate_machine_code(Opcode.MOV,Registers.A,Registers.G1))
              code += count_expresion(left_expression,labels,line,line_num)
-             if (operand == '=='): 
-                 code.append(generate_machine_code(Opcode.JMPE,Registers.G1,Registers.A))  
+             if (operand == '=='):
+                code.append(generate_machine_code(Opcode.JMPE,Registers.G1,Registers.A))
+             elif (operand == '!='):
+                 code.append(generate_machine_code(Opcode.JNE,Registers.G1,Registers.A))
              else:
                  code.append(generate_machine_code(Opcode.JMPL,Registers.G1,Registers.A)) 
              # Добавляем jmp без определённого адресса чтобы потом его переопределить! 
              code.append(generate_machine_code(Opcode.JMP,addr=0xFFFFFF))
-             branch_stack.append((symbols[0],len(code) - 1))
+             branch_stack.append((symbols[0],len(code) - 1,comeback))
         elif symbols[0] == '}':
              if (len(branch_stack) == 0): 
                 raise ValueError(f"{line}\nInvalid syntax")
-             brench_type,code_addr = branch_stack.pop(0)
+             brench_type,code_addr,comeback = branch_stack.pop(-1)
              if (brench_type == "while"):
                  # Делаем зацикливание, прыжок на проверку условия цикла
-                 code.append(generate_machine_code(Opcode.JMP,addr=code_addr-1))
+                 code.append(generate_machine_code(Opcode.JMP,addr=comeback))
              code[code_addr] = generate_machine_code(Opcode.JMP,addr=len(code))
+              
         elif symbols[0] == "print":
             if (len(symbols) != 4):
                 raise ValueError(f"{line}\nIncorrect expresion on line {line_num}")
@@ -240,8 +294,10 @@ def translate(text):
             if (not to_print in labels):
                 raise ValueError(f"{line}\nNo such string declared line {line_num}")
             if type_of_labels[labels.index(to_print)] == 'int':
-                pass
-                #TODO добавить возможность выводить числа
+                #Умеет печатать только цифры!!
+                code.append(generate_machine_code(Opcode.LOAD,Registers.G1,None,addr=labels.index(to_print)))
+                code.append(generate_machine_code(Opcode.STORE,Registers.G1,None,addr=1))
+
             elif type_of_labels[labels.index(to_print)] == 'char':
                 code.append(generate_machine_code(Opcode.LOAD,Registers.G1,None,addr=labels.index(to_print)))
                 code.append(generate_machine_code(Opcode.LOADIM,Registers.G2,None,val=labels.index(to_print)))
@@ -253,33 +309,68 @@ def translate(text):
                 code.append(generate_machine_code(Opcode.JMP,addr=len(code)+6))
                 #BR++
                 code.append(generate_machine_code(Opcode.ADD,Registers.BR,Registers.G3))
-                code.append(generate_machine_code(Opcode.ADD,Registers.G2,Registers.BR))
-                code.append(generate_machine_code(Opcode.LOAD,Registers.G4,Registers.G2))
+                code.append(generate_machine_code(Opcode.ADD,Registers.G2,Registers.G3))
+                code.append(generate_machine_code(Opcode.LOAD,Registers.G2,Registers.G4))
                 code.append(generate_machine_code(Opcode.STORE,Registers.G4,None,addr=1))
                 code.append(generate_machine_code(Opcode.JMP,addr=len(code)-6))
+            elif type_of_labels[labels.index(to_print)] == 'I':
+                code.append(generate_machine_code(Opcode.LOAD,Registers.B,None,addr=0))
+                code.append(generate_machine_code(Opcode.STORE,Registers.B,None,addr=1))
             else:
-                raise ValueError(f"{line}\nYou trying to use IO port as an argument!{line_num}")
+                raise ValueError(f"{line}\nYou trying to use out port as an argument!{line_num}")
         elif symbols[0] == 'interruption':
             code.append(generate_machine_code(Opcode.HALT))
-            code.append(generate_machine_code(Opcode.LOADIM,Registers.INT,None,val = len(code)))
+            code[0] = generate_machine_code(Opcode.LOADIM,Registers.INT,None,val = len(code))
+
+            #Сохраняем состояние
+            for i in range(10):
+                code.append(generate_machine_code(Opcode.STORE,Registers(i),None,addr=i+2))
             int_func_present = True
     if int_func_present:    
-       code.append(generate_machine_code(Opcode.MOV,Registers.PC,Registers.INTPC))
+       #Возвращаем состояние
+        for i in range(10):
+            code.append(generate_machine_code(Opcode.LOAD,Registers(i),None,addr=i+2))
+        code.append(generate_machine_code(Opcode.MOV,Registers.PC,Registers.INTPC))
     else:
-       code.append(generate_machine_code(Opcode.HALT))
+        code.append(generate_machine_code(Opcode.HALT))
         
             
-    return code    
+    return [code,labels]    
 
-             
+def code_to_bin(code, bin_filename, hex_filename):
 
+    byte_data = bytearray()
+    for hex_str in code:
+        if len(hex_str) != 8:
+            raise ValueError(f"Hex string '{hex_str}' must be 8 characters long")
+        byte_data.extend(bytes.fromhex(hex_str))
+
+    with open(bin_filename+'.bin', 'wb') as bin_file:
+        bin_file.write(byte_data)
+
+    with open(hex_filename+'.hex', 'w') as hex_file:
+        for i in range(0, len(byte_data), 16):
+            chunk = byte_data[i:i+16]
+            hex_line = ' '.join(f"{byte:02X}" for byte in chunk)
+            hex_file.write(hex_line + '\n')
+    
+    print(f"Saved {len(byte_data)} bytes to {bin_filename} (binary) and {hex_filename} (hex)")
+
+def get_code(file_name):
+    program = add_spaces(read_file(file_name))
+    code,labels = translate(program)
+    return code,labels
             
 
 if __name__ == "__main__":
     print("TRANSLATING!")
-    program = add_spaces(read_file("test1.alg"))
-    #print(program)
-    
-    code = translate(program)
-    for c in range(len(code)):
-        print(c,code[c])
+    file_name = sys.argv[1]
+    out_file  = sys.argv[2]
+    program = add_spaces(read_file(file_name))
+    code,labels = translate(program)
+
+    with open (out_file+".txt",'w') as res:
+        for c in range(len(code)):
+            res.write(f"{c}. {code[c]} - {machine_to_opcode[int(code[c][0],16)].name}\n")
+    code_to_bin(code,out_file,out_file)
+    print("TRANSLATING OVER!")
